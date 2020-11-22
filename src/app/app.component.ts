b1171a56e56dce522c215c086ffc3d4c95f26c7c
Axel Alexis Montejo Lopez <alemontejo.lp@gmail.com>
2020-11-21 23:12:05 -0500

    Project base implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'uccafe-frontend';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    document.onreadystatechange = this.setSidenavPosition
+    window.onresize = this.setSidenavPosition
+  }
+
+  setSidenavPosition() {
+    let toolbar = document.getElementById('toolbar')
+    let sidenav = document.getElementById('sidenav')
+    sidenav.style.top = `${toolbar.offsetHeight}px`
+  }
 }
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu'; 
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';   
+
+let modules = [
+  MatToolbarModule, 
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatMenuModule,
+  MatCardModule,
+  MatSelectModule,
+  MatAutocompleteModule
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule { }
